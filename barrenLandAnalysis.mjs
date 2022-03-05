db4ab9fafc80678fb54a21abfa548eeeff6e628e
CJ Barnes <86207741+cee-jay-bee@users.noreply.github.com>
2022-03-05 14:17:20 -0600

    calculuates area correctly, however needs to be output as string, and handle if no fertile land

--- barrenLandAnalysis.mjs
+++ barrenLandAnalysis.mjs
@@ -1,7 +1,8 @@
-import Coordinates from './coordinatesClass.mjs';
 
 const STDIN = ['0 292 399 307'];
 const STDIN2 = ['48 192 351 207', '48 392 351 407', '120 52 135 547', '260 52 275 547'];
+const STDIN3 = ['0 3 9 6'];
+
 
 const findFertileLand = (givenCornersString) => {
     const tileSize = 1;
@@ -9,9 +10,7 @@ const findFertileLand = (givenCornersString) => {
     const length = 600;
 
     let xTiles = width / tileSize;
-    let yTiles = length /tileSize;
-
-    let grid = [];
+    let yTiles = length / tileSize;
 
     // transform given string coordinates into arrays of integers
     // input of provided strings in arrays
@@ -37,40 +36,105 @@ const findFertileLand = (givenCornersString) => {
         let coveredBarrenLandCoordinates = [];
 
         for (let x = cornersOfBarrenLand[0]; x <= cornersOfBarrenLand[2]; x++) {
-            for (let y = cornersOfBarrenLand[1]; y <= cornersOfBarrenLand[3]; y++) {
-                let currentCoordinates = new Coordinates(x, y);
-                coveredBarrenLandCoordinates.push(currentCoordinates);
+            for (let y = cornersOfBarrenLand[1]; y <= cornersOfBarrenLand[3]; y++) {;
+                coveredBarrenLandCoordinates.push([x,y]);
             }
         }
         return coveredBarrenLandCoordinates;
     }
-    
-    
-    let barrenLandCoordinatesArrays = barrenLandCoordinates(givenCornersString);
-    let barrenLandRectangles = [];
-    
-    for (let i = 0; i < barrenLandCoordinatesArrays.length; i++){
-        barrenLandRectangles.push(landOfCoveredRectangleFromCoordinates(barrenLandCoordinatesArrays[i]));
+
+    //creates grid based on decided size and sets as fertile (1)
+    const createGrid = () => {
+        let newGrid = []
+        for(let i = 0; i < xTiles; i++){
+            newGrid[i] = [];
+            for (let j = 0; j < yTiles; j++){
+                newGrid[i][j] = 1;         
+            }
+        }
+        return newGrid;
     }
-
-    console.log(barrenLandRectangles);
     
-    for (let y = 0; y < yTiles; y++){
-        for (let x = 0; x < xTiles; x++){
-            let newPlotOfLand = new Coordinates(x, y);
-
-            for (let i = 0; i < barrenLandRectangles.length; i++){
-                if (newPlotOfLand.xValue === barrenLandRectangles[i].x && newPlotOfLand.yValue() === barrenLandRectangles[i].y ){
-                    newPlotOfLand.setBarren(true);
-                } else {
-                    newPlotOfLand.setBarren(false);
+    // recursive function to calculate the Fertile Land Area based on checking to see which piece of land is fertile and rechecking each neighbor
+    // calls a breadth search that initiates a search of all of the neighbors of that particular plot land and changes them to barren after check
+    const calculateFertileLandArea = (xValue, yValue) => {
+        for (let y = yValue; y < yTiles; y++) {
+            for (let x = xValue; x < xTiles; x++){
+                if (grid[x][y] === 1){
+                    console.log('x', x, 'y', y);
+                    let currentFertileArea = breadthSearch(x, y);
+                    fertileLand.push(currentFertileArea);
+                    calculateFertileLandArea(x, y);
                 }
             }
-            grid.push(newPlotOfLand);
         }
     }
 
-    console.log(grid);
+    const breadthSearch = (x, y) => {
+        let countOfArea = 0;
+        let queueToCheck = [[x, y]];
+
+        while(queueToCheck.length){
+            let currentCoord = queueToCheck.pop();
+            if(grid[currentCoord[0]][currentCoord[1]] === 1) {
+                countOfArea += 1;
+                grid[currentCoord[0]][currentCoord[1]] = 0;
+                if( currentCoord[0] - 1 >= 0 && grid[currentCoord[0] - 1][currentCoord[1]] === 1){
+                    queueToCheck.push([currentCoord[0]- 1, currentCoord[1]]);
+                } 
+                if( currentCoord[0] + 1 < xTiles && grid[currentCoord[0] + 1][currentCoord[1]] === 1){
+                    queueToCheck.push([currentCoord[0] + 1, currentCoord[1]]);
+                }
+                if( currentCoord[1] - 1 >= 0 && grid[currentCoord[0]][currentCoord[1] - 1] === 1){
+                    queueToCheck.push([currentCoord[0], currentCoord[1] - 1]);
+                }
+                if( currentCoord[1] + 1 < yTiles && grid[currentCoord[0]][currentCoord[1] + 1] === 1){
+                    queueToCheck.push([currentCoord[0], currentCoord[1] + 1]);
+                }
+
+            }
+        }
+        return countOfArea;
+    }
+    
+    // create array to store answers to fertile land calculations
+    let fertileLand = [];
+
+    // create base grid with all fertile land
+    let grid = createGrid();
+    
+    // change incoming string of barren land coordinates to arrays of integers
+    let barrenLandCoordinatesArrays = barrenLandCoordinates(givenCornersString);
+    
+    // set array to contain barren land coordinate squares
+    let barrenLandRectangles = [];
+    
+    // loop through barren land coordinates and store all barren land in array
+    for (let i = 0; i < barrenLandCoordinatesArrays.length; i++) {
+        barrenLandRectangles.push(landOfCoveredRectangleFromCoordinates(barrenLandCoordinatesArrays[i]));
+    }
+    
+    // create the barren grid by looping through the grids and changing fertile (1) to barren (0)
+    for (let y = 0; y < yTiles; y++){
+        for (let x = 0; x < xTiles; x++) {
+            for (let i = 0; i < barrenLandRectangles.length; i++){
+                for (let j = 0; j < barrenLandRectangles[i].length; j++){
+                    if (x === barrenLandRectangles[i][j][0] && y === barrenLandRectangles[i][j][1] ){
+                        grid[x][y] = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    // calculate the total fertile land
+    calculateFertileLandArea(0, 0);
+    fertileLand.sort(function(a, b) {
+        return a - b;
+    });
+    console.log(fertileLand);
 } 
 
 findFertileLand(STDIN);
+
+export default findFertileLand;
